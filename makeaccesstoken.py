a9f965c5e8215eccdff44cc22f46243a70ace947
A2Pro <87340157+A2Pro@users.noreply.github.com>
2024-01-24 09:26:02 -0800

    Create makeaccesstoken.py
    DOn't forget to replace keys with your own.

---
+++ makeaccesstoken.py
@@ -0,0 +1,84 @@
+import plaid
+from plaid.api import plaid_api
+import base64
+import os
+import datetime as dt
+import json
+import time
+
+from dotenv import load_dotenv
+from flask import Flask, request, jsonify
+import plaid
+from plaid.model.payment_amount import PaymentAmount
+from plaid.model.payment_amount_currency import PaymentAmountCurrency
+from plaid.model.products import Products
+from plaid.model.sandbox_public_token_create_request import SandboxPublicTokenCreateRequest
+from plaid.model.country_code import CountryCode
+from plaid.model.link_token_get_request import LinkTokenGetRequest
+from plaid.model.recipient_bacs_nullable import RecipientBACSNullable
+from plaid.model.payment_initiation_address import PaymentInitiationAddress
+from plaid.model.payment_initiation_recipient_create_request import PaymentInitiationRecipientCreateRequest
+from plaid.model.payment_initiation_payment_create_request import PaymentInitiationPaymentCreateRequest
+from plaid.model.payment_initiation_payment_get_request import PaymentInitiationPaymentGetRequest
+from plaid.model.link_token_create_request_payment_initiation import LinkTokenCreateRequestPaymentInitiation
+from plaid.model.item_public_token_exchange_request import ItemPublicTokenExchangeRequest
+from plaid.model.link_token_create_request import LinkTokenCreateRequest
+from plaid.model.link_token_create_request_user import LinkTokenCreateRequestUser
+from plaid.model.asset_report_create_request import AssetReportCreateRequest
+from plaid.model.asset_report_create_request_options import AssetReportCreateRequestOptions
+from plaid.model.asset_report_user import AssetReportUser
+from plaid.model.asset_report_get_request import AssetReportGetRequest
+from plaid.model.asset_report_pdf_get_request import AssetReportPDFGetRequest
+from plaid.model.auth_get_request import AuthGetRequest
+from plaid.model.transactions_sync_request import TransactionsSyncRequest
+from plaid.model.identity_get_request import IdentityGetRequest
+from plaid.model.investments_transactions_get_request_options import InvestmentsTransactionsGetRequestOptions
+from plaid.model.investments_transactions_get_request import InvestmentsTransactionsGetRequest
+from plaid.model.accounts_balance_get_request import AccountsBalanceGetRequest
+from plaid.model.accounts_get_request import AccountsGetRequest
+from plaid.model.investments_holdings_get_request import InvestmentsHoldingsGetRequest
+from plaid.model.item_get_request import ItemGetRequest
+from plaid.model.institutions_get_by_id_request import InstitutionsGetByIdRequest
+from plaid.model.transfer_authorization_create_request import TransferAuthorizationCreateRequest
+from plaid.model.transfer_create_request import TransferCreateRequest
+from plaid.model.transfer_get_request import TransferGetRequest
+from plaid.model.transfer_network import TransferNetwork
+from plaid.model.transfer_type import TransferType
+from plaid.model.transfer_authorization_user_in_request import TransferAuthorizationUserInRequest
+from plaid.model.ach_class import ACHClass
+from plaid.model.transfer_create_idempotency_key import TransferCreateIdempotencyKey
+from plaid.model.transfer_user_address_in_request import TransferUserAddressInRequest
+from plaid.api import plaid_api
+# Available environments are
+# 'Production'
+# 'Development'
+# 'Sandbox'
+configuration = plaid.Configuration(
+    host=plaid.Environment.Sandbox,
+    api_key={
+        'clientId': "YOUR_CLIENT_ID",
+        'secret': "YOUR_SECRET",
+    }
+)
+
+api_client = plaid.ApiClient(configuration)
+client = plaid_api.PlaidApi(api_client)
+
+# Account filtering isn't required here, but sometimes 
+# it's helpful to see an example. 
+
+import plaid
+from plaid.model.item_public_token_exchange_request import ItemPublicTokenExchangeRequest
+
+pt_request = SandboxPublicTokenCreateRequest(
+    institution_id="ins_13",
+    initial_products=[Products('transactions')]
+)
+pt_response = client.sandbox_public_token_create(pt_request)
+# The generated public_token can now be
+# exchanged for an access_token
+exchange_request = ItemPublicTokenExchangeRequest(
+    public_token=pt_response['public_token']
+)
+exchange_response = client.item_public_token_exchange(exchange_request)
+print(exchange_response)
